7548ac7c857c4f691094e7d9e26309d0813dea65
Hideyuki191 <hideyuki1000000@gmail.com>
2023-02-12 20:53:48 +0800

    Added Player Controls
    Player can now move around and jump
    
    For jump detecting, we use raycast to get distance from bottom of player to ground and if the ground is close enough, we let player jump

---
+++ sp4/Assets/MoveScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float Spd = 12f;
+    public bool Jump = false;
+
+    public float GroundDistance = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GroundDistance = gameObject.GetComponent<CapsuleCollider>().radius;
+    }
+
+    public bool IsGrounded() // Check if player is close or touching a grounded area
+    {
+        Vector3 origin = transform.position;
+        origin.y -= gameObject.GetComponent<CapsuleCollider>().radius; //Minus by size of radius to move origin down to bottom part of player
+
+        Vector3 direction = -Vector3.up; //Direction that points down on the y axis to only check the ground
+        Ray ray = new Ray(origin, direction);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) //if sees ground
+        {
+            float distance = hit.distance; //Get the distance from the ray to the landable area
+
+            Debug.DrawRay(origin, direction * distance, Color.red);
+
+            if (distance <= (gameObject.GetComponent<CapsuleCollider>().radius + 0.1f)) //If distance from bottom of player to ground is close enough
+            {
+                return true; //Let Player jump
+            }
+            else
+                return false;
+        }
+
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z) * Spd * Time.deltaTime;
+
+        gameObject.transform.position += move;
+
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 250, 0));
+        }
+    }
+}
---
+++ sp4/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform PlayerBox;
+
+    float RotationX = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseControls();
+    }
+
+    private void MouseControls()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * 100f * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * 100f * Time.deltaTime;
+
+        RotationX -= MouseY;
+        RotationX = Mathf.Clamp(RotationX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(RotationX, 0f, 0f);
+        PlayerBox.Rotate(Vector3.up * MouseX);
+
+    }
+}
