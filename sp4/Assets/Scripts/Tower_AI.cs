823f6f86a9e3f806466f6c8a7a3308b99e66bba1
Hideyuki191 <hideyuki1000000@gmail.com>
2023-02-13 10:19:50 +0800

    Added TMP

--- sp4/Assets/Scripts/Tower_AI.cs
+++ sp4/Assets/Scripts/Tower_AI.cs
@@ -13,7 +13,7 @@ public class Tower_AI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
