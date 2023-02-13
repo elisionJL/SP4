a8f137b2ff98e8d9ec6485cc108d7e41da77eb1f
Hideyuki191 <hideyuki1000000@gmail.com>
2023-02-13 09:14:33 +0800

    Scripts moved and added new script

---
+++ sp4/Assets/Scripts/Tower_AI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_AI : MonoBehaviour
+{
+    public Transform Player;
+    public float maxAngle;
+    public float maxRadius;
+
+    private bool isInFov = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        isInFov = inFov(transform, Player, maxAngle, maxRadius);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
+
+        Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, fovLine1);
+        Gizmos.DrawRay(transform.position, fovLine2);
+
+        if(!isInFov)
+        {
+            Gizmos.color = Color.red;
+        }
+
+        else
+        {
+            Gizmos.color = Color.green;
+        }
+
+        Gizmos.DrawRay(transform.position, (Player.position - transform.position).normalized * maxRadius);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
+    }
+
+    public static bool inFov (Transform checkingObject, Transform target, float maxAngle, float maxRadius)
+    {
+        Collider[] overlaps = new Collider[10];
+        int count = Physics.OverlapSphereNonAlloc(checkingObject.position, maxRadius, overlaps);
+
+        for (int i = 0; i < count + 1; i++)
+        {
+            if(overlaps[i] != null)
+            {
+                if(overlaps[i].transform == target)
+                {
+                    Vector3 directionBetween = (target.position - checkingObject.position).normalized;
+                    directionBetween.y *= 0;
+
+                    float angle = Vector3.Angle(checkingObject.forward, directionBetween);
+
+                    if(angle <= maxAngle)
+                    {
+                        Ray ray = new Ray(checkingObject.position, target.position - checkingObject.position);
+                        RaycastHit hit;
+
+                        if(Physics.Raycast(ray, out hit, maxRadius))
+                        {
+                            if (hit.transform == target)
+                                return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
