57c6c2f1e79043eda727ef6deb4b7199171d579c
VEXFLEX <210205H@mymail.nyp.edu.sg>
2023-02-15 09:46:33 +0800

    Merge

--- sp4/Assets/Scripts/Tower_AI.cs
+++ sp4/Assets/Scripts/Tower_AI.cs
@@ -29,8 +29,8 @@ public class Tower_AI : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if(Player == null && GameObject.FindGameObjectsWithTag("Enemy").Length > 0)
-            Player = GameObject.FindGameObjectWithTag("Enemy").transform;
+        if(playerTransform == null && GameObject.FindGameObjectsWithTag("Enemy").Length > 0)
+            playerTransform = GameObject.FindGameObjectWithTag("Enemy").transform;
         else
             isInFov = inFov(transform, playerTransform, maxAngle, maxRadius);
 
