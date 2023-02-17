29a44b616b9257c53198d99df81d2edfb7778b95
VEXFLEX <210205H@mymail.nyp.edu.sg>
2023-02-17 10:26:36 +0800

    Added waypoint prefab

--- sp4/Assets/Scripts/waypoints/SpawnerOfArrows.cs
+++ sp4/Assets/Scripts/waypoints/SpawnerOfArrows.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnerOfArrows : MonoBehaviour
 {
     public List<Transform> Waypoints;
+    private float timer;
+    public GameObject Arrows;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,16 @@ public class SpawnerOfArrows : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            for (int i = 0; i < Waypoints.Count - 1; ++i)
+            {
+                Arrows.transform.position = Waypoints[i].position;
+                Arrows.GetComponent<Arrow>().AssignWaypoints(Waypoints[i + 1]);
+                Instantiate(Arrows);
+            }
+            timer = 0;
+        }
     }
 }
