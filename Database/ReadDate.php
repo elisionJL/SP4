2388bc8b0f3937e5bd57dea6a9c36666f04c62bc
VEXFLEX <210205H@mymail.nyp.edu.sg>
2023-02-22 15:54:39 +0800

    Fixed archer animation and pushed ReadDate

---
+++ Database/ReadDate.php
@@ -0,0 +1,33 @@
+<?php //ReadScoreboard.php
+// Connect database
+include("dbconninc.php");
+// Prepare Statement (SQL query)
+if(!isset($_POST["username"]))die("not posted!");
+$sPlayerName=$_POST["username"];
+
+$query="select Username from tb_playerstats where Username=?";
+$stmt=$conn->prepare($query);
+$stmt->bind_param("s", $sPlayerName);
+$stmt->execute();
+$stmt->store_result();
+$row=$stmt->num_rows();
+$stmt->bind_result($sPlayerName);
+$stmt->fetch();
+if($row==0){
+    echo "0";
+}
+else{
+    $query2="select LastLogin from tb_playerstats where Username=?";
+    $stmt=$conn->prepare($query2);
+    //s - string, i - integer...make sure it matches the data types!
+    $stmt->bind_param("s", $sPlayerName);
+    // Execute Statement
+    $stmt->execute();
+    //Link results to variables
+    $stmt->bind_result($sPlayerSkins);
+    $stmt->fetch();
+    echo $sPlayerSkins;
+}
+$stmt->close(); // Close Statement
+$conn->close();
+?>
